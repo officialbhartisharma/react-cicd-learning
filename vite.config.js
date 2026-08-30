eb2ec0d7de7405258b8b337ce1a7eabd833bc257
Bharti <bhartisharmaofficial912@gmail.com>
2026-08-30 17:35:51 +0530

    fix: use root base issue in vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-cicd-learning/',
+  // base: '/react-cicd-learning/', // Setting when CI/CD with GitHub Pages
+  base: '/', // Setting when CI/CD with Jenkins and Netlify or any other platforms
   plugins: [react()],
 })
